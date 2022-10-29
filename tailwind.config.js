8e0e570c7c6d076b8c3571acd1ac78ad5f1e07b2
Tanish Hire <83215038+tanishhire@users.noreply.github.com>
2022-10-29 20:34:46 +0530

    Major changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,21 @@
-// /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ['./*.html'],
   theme: {
-    extend: {
-      fontFamily: {
-         poppins: ['Poppins'],
-      }
-      },
-    screens : {
-      sm : '480px',
-      md : '768px',
-      lg : '976px',
-      xl : '1440px'
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
     },
     extend: {
-      colors : {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
         darkBlue: 'hsl(228, 39%, 23%)',
-        celestialBlue : 'hsl(206, 61%, 56%)',
-        blueJeans: 'hsl(195, 74%, 62%)',
-        middleBlueGreen: 'hsl(181, 43%, 67%)',
-        seaFoamGreen: 'hsl(158, 38%, 74%)',
-        jetStream: 'hsl(144, 35%, 80%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
         veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
         veryLightGray: 'hsl(0, 0%, 98%)',
       },
     },
